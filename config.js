e10eff44fe1ea3ea7dfc277236888d9c487b059e
dohiep01 <87643548+dohiep01@users.noreply.github.com>
2021-07-19 16:28:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Mai candy à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hihi, hát cho teo nghe đi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Hát luôn nè :333',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón đi uống bia nhé.',
+    question:'Nghe ca sĩ hát nè <3',
+    btnReply: 'Gửi cho anh bn đẹp trai <3',
+    reply: 'Nghe thì nghe mà không nghe thì nghe <33333333',
+    mess: 'T biết mà 🥰. U mê giọng hát tóa  😘😘',
+    messDesc: 'Tối nay 10h call hát tiếp nhé :333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
